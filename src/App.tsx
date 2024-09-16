1e1137830c1aba094edfd6f304cc6f65c3788a46
Rodolpho Reis <rodolphoreis@live.com>
2024-09-16 15:07:40 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main className="container max-w-full">
+      <div className="bg-red-500 max-w-screen-xl mx-auto my-0">
+        <h1 className="text-center text-white font-bold sm:text-xs mg:text-xl xl:text-3xl">
+          Vite React App
+        </h1>
+        <p className="text-center text-white text-lg">
+          Welcome to your Vite React app!
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
